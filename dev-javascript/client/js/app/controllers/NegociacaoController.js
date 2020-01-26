bca1ffe284e2ee93085401b4d491f86a4bc0b263
Felipe <felipesaev@gmail.com>
2020-01-26 14:33:34 -0300

    pegando mais de uma requisição

--- dev-javascript/client/js/app/controllers/NegociacaoController.js
+++ dev-javascript/client/js/app/controllers/NegociacaoController.js
@@ -26,20 +26,38 @@ class NegociacaoController {
         this._limpaFormulario();   
     }
 
-   importaNegociacoes() {
+    importaNegociacoes() {
 
-  let service = new NegociacaoService();
-
-  service.obterNegociacoesDaSemana((erro,negociacoes) => {
-      if(erro) {
-          this._mensagem.texto = erro;
-          return;
-      }
-
-      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-      this._mensagem.texto = 'Negociações importadas com sucesso';
-  });
-}
+        let service = new NegociacaoService();
+    
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+    
+            if(erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+    
+            service.obterNegociacoesDaSemanaAnterior((erro, negociacoes) => {
+    
+                if(erro) {
+                    this._mensagem.texto = erro;
+                    return;
+                }
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+    
+                service.obterNegociacoesDaSemanaRetrasada((erro, negociacoes) => {
+    
+                    if(erro) {
+                        this._mensagem.texto = erro;
+                        return;
+                    }
+                    negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso';
+                });
+            });
+        });
+    }
     
     apaga() {
         
