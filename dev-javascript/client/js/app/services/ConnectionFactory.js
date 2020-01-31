07439b895470451f4985bebbdb9af3310015f317
Felipe <felipesaev@gmail.com>
2020-01-31 00:41:34 -0300

    criando um Monkey patching na classe ConnectionFactory

--- dev-javascript/client/js/app/services/ConnectionFactory.js
+++ dev-javascript/client/js/app/services/ConnectionFactory.js
@@ -1,9 +1,10 @@
 var ConnectionFactory = (function() {
 
-        var stores = ['negociacoes'];
-        var version = 4;
-        var dbName = 'aluraframe';
+        const stores = ['negociacoes'];
+        const version = 4;
+        const dbName = 'aluraframe';
         var connection = null;
+        var close = null;
 
         return class ConnectionFactory {
             constructor() {
@@ -17,8 +18,16 @@ var ConnectionFactory = (function() {
                         ConnectionFactory._createStores(e.target.result);
                     };
                     openRequest.onsuccess = e => {
-                        resolve(connection);
-                    };
+                        if(!connection) {
+                            connection = e.target.result;   
+                            close = connection.close.bind(connection);
+                            connection.close = function() {
+                                throw new Error ('Você não pode fechar uma conexão')
+                            }
+                        }
+                    resolve(connection);
+                    
+                 };
 
                     openRequest.onerror = e => {
                         reject(e.target.error.name);
@@ -31,6 +40,11 @@ var ConnectionFactory = (function() {
                     if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
                     connection.createObjectStore(store, { autoIncrement: true });
                 });
+            }
+
+            static closeConnection() {
+                close();            
+                connection = null;
             }
         }
 
